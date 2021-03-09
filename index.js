391e45ef0564f7dd03b0f39308c50765b9dced6b
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-03-09 13:58:24 +0000

    refactoring for @AlexKamaev suggestions

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ const testCafeArguments = getInput('args');
 const version   = getInput('version');
 const branch    = getInput('branch');
 const commit    = getInput('commit');
-let skipInstall = getInput('skip-install')
-if (skipInstall === 'true') {
-    skipInstall = true;
-}
-if (skipInstall !== true) {
-    skipInstall = false;
-}
+const skipInstall = getInput('skip-install') === true
 const branchCmd = branch && !commit ? `-b ${branch}` : '';
 
 const gitCloneCmd    = `git clone https://github.com/DevExpress/testcafe.git ${branchCmd}`;
@@ -38,21 +32,19 @@ log(`COMMIT: ${getInputStr(commit)}`);
 log(`SKIP INSTALL: ${skipInstall}`);
 
 if (branch || commit) {
-    if (!skipInstall) {
-        log('Cloning the TestCafe repository...');
-        log(gitCloneCmd);
-        execSync(gitCloneCmd, { stdio: 'inherit' });
-    
-        log('Checking out the repository...');
-        log(gitCheckoutCmd);
-        execSync(gitCheckoutCmd, { stdio: 'inherit' });
-    
-        log('Installing npm packages...');
-        execSync(`cd testcafe && npm install `, { stdio: 'inherit' });
-    
-        log('Building TestCafe...');
-        execSync(`cd testcafe && npx gulp fast-build`, { stdio: 'inherit' });
-    }
+    log('Cloning the TestCafe repository...');
+    log(gitCloneCmd);
+    execSync(gitCloneCmd, { stdio: 'inherit' });
+
+    log('Checking out the repository...');
+    log(gitCheckoutCmd);
+    execSync(gitCheckoutCmd, { stdio: 'inherit' });
+
+    log('Installing npm packages...');
+    execSync(`cd testcafe && npm install `, { stdio: 'inherit' });
+
+    log('Building TestCafe...');
+    execSync(`cd testcafe && npx gulp fast-build`, { stdio: 'inherit' });
     testCafeCmd = 'node testcafe/bin/testcafe';
 }
 else {
