4ad519b7c79945e1d7feb3039ffb03df2356188b
David Goemans <dgoemans@gmail.com>
2021-11-10 10:50:33 +0100

    Add `working-directory` as an option to the action

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const branch      = getInput('branch');
 const commit      = getInput('commit');
 const skipInstall = getInput('skip-install') === 'true';
 const branchCmd   = branch && !commit ? `-b ${branch}` : '';
+const cwd         = getInput('working-directory');
 
 const gitCloneCmd    = `git clone https://github.com/DevExpress/testcafe.git ${branchCmd}`;
 const gitCheckoutCmd = `git -C testcafe checkout ${commit}`;
@@ -34,23 +35,23 @@ log(`SKIP INSTALL: ${skipInstall}`);
 if (branch || commit) {
     log('Cloning the TestCafe repository...');
     log(gitCloneCmd);
-    execSync(gitCloneCmd, { stdio: 'inherit' });
+    execSync(gitCloneCmd, { stdio: 'inherit', cwd });
 
     log('Checking out the repository...');
     log(gitCheckoutCmd);
-    execSync(gitCheckoutCmd, { stdio: 'inherit' });
+    execSync(gitCheckoutCmd, { stdio: 'inherit', cwd });
 
     log('Installing npm packages...');
-    execSync(`cd testcafe && npm install `, { stdio: 'inherit' });
+    execSync(`cd testcafe && npm install `, { stdio: 'inherit', cwd });
 
     log('Building TestCafe...');
-    execSync(`cd testcafe && npx gulp fast-build`, { stdio: 'inherit' });
+    execSync(`cd testcafe && npx gulp fast-build`, { stdio: 'inherit', cwd });
     testCafeCmd = 'node testcafe/bin/testcafe';
 }
 else {
     if (!skipInstall) {
         log('Installing TestCafe from npm...');
-        execSync(`npm i testcafe@${version}`);
+        execSync(`npm i testcafe@${version}`, { cwd });
     }
     testCafeCmd = 'npx testcafe';
 }
@@ -61,4 +62,4 @@ if (os.type() === 'Linux')
     xvfbCmd = `xvfb-run --server-args="-screen 0 1280x720x24" `;
 
 log('Running TestCafe...');
-execSync(`${xvfbCmd}${testCafeCmd} ${testCafeArguments}`, { stdio: 'inherit' });
+execSync(`${xvfbCmd}${testCafeCmd} ${testCafeArguments}`, { stdio: 'inherit', cwd });
